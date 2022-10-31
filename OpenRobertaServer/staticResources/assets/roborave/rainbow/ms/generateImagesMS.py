9210f87f6b3f3f1fe003ce7c78199bffd6b322d8
AetherDim <74829006+AetherDim@users.noreply.github.com>
2022-10-31 13:40:41 +0100

    Implemented inkscape version check

--- OpenRobertaServer/staticResources/assets/roborave/rainbow/ms/generateImagesMS.py
+++ OpenRobertaServer/staticResources/assets/roborave/rainbow/ms/generateImagesMS.py
@@ -4,6 +4,7 @@ import copy
 import subprocess
 import os
 from multiprocessing import Process
+import re
 
 red = "#e40303"
 orange = "#ff8c00"
@@ -19,6 +20,17 @@ width = 3200
 height = 2160
 PROCESS_COUNT = 32
 
+INKSCAPE_VERSION_IS_OLD = True
+
+def isInkscapeVersionOld():
+    if not hasattr(isInkscapeVersionOld, "INKSCAPE_VERSION_IS_OLD"):
+        txt = subprocess.check_output(["inkscape", "--version"])
+        res = re.search(r"Inkscape (\d+)\.(\d+)\.(\d+)", str(txt))
+        isInkscapeVersionOld.INKSCAPE_VERSION_IS_OLD = (int(res.group(1)) == 0)
+        print("Inkscape version is " + "old" if isInkscapeVersionOld.INKSCAPE_VERSION_IS_OLD else "new")
+
+    return isInkscapeVersionOld.INKSCAPE_VERSION_IS_OLD
+
 def recursiveReplace(child, colormap):
     if len(child) != 0:
         for c in child:
@@ -28,7 +40,6 @@ def recursiveReplace(child, colormap):
     if replace in child.attrib and child.attrib[replace] in colormap:
         print(child.attrib[replace] + " -> " + colormap[child.attrib[replace]])
         child.set(replace, colormap[child.attrib[replace]])
-
 
 def generatePermutation(name, tree, i, c):
     colormap = dict(zip(colors, c))
@@ -41,7 +52,10 @@ def generatePermutation(name, tree, i, c):
     with open(svgName, "wb") as f:
         svg.write(f)
 
-    subprocess.run(["inkscape", '--export-type=png', f'--export-filename={name+str(i)+".png"}', f'--export-width={width}', f'--export-height={height}', svgName])
+    if isInkscapeVersionOld():
+        subprocess.run(["inkscape", '--export-png', f'{name+str(i)+".png"}', f'--export-width={width}', f'--export-height={height}', svgName])
+    else:
+        subprocess.run(["inkscape", '--export-type=png', f'--export-filename={name+str(i)+".png"}', f'--export-width={width}', f'--export-height={height}', svgName])
 
     os.remove(svgName)
 
@@ -68,6 +82,6 @@ def generatePermutations(name):
                     p.join()
                     processList.remove(p)
 
-if __name__ == '__main__':                
+if __name__ == '__main__':
     generatePermutations("dino")
     generatePermutations("cloud")
